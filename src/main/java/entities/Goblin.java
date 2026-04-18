45416a74b9941eed92854d9a137a7729e0704c56
GorSargsyan2004 <sargsyangor2004@gmail.com>
2026-04-18 13:02:33 +0400

    Added Goblin, bags persist

---
+++ src/main/java/entities/Goblin.java
@@ -0,0 +1,123 @@
+package entities;
+
+import animations.Animation;
+import animations.Direction;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+
+import static main.Game.SCALE;
+import static utils.Constants.GoblinConstants.*;
+
+public class Goblin extends Enemy {
+
+    Goblin(int health, int damage, Point2D.Double pos, double movementSpeed, int[][] lvlData) {
+        super(health, damage, pos, movementSpeed, lvlData);
+
+        this.attackDistance = (int) (15 * SCALE);
+
+        this.entityHeight = 35;
+        this.entityWidth = 35;
+
+        this.xDrawOffset = 10;
+        this.yDrawOffset = 10;
+
+        initAnimations();
+
+        initHitbox();
+    }
+
+    private void initAnimations() {
+        String[] anims = {"Take_Hit", "Death", "Idle", "Run", "Attack"};
+
+        animations = new Animation[anims.length];
+        for (int i = 0; i < anims.length; i++) {
+            if (i < 3)
+                animations[i] = new Animation("/Enemy/Goblin/"+anims[i]+".png", 58, 65, entityHeight, entityWidth, 4, 115);
+            else animations[i] = new Animation("/Enemy/Goblin/"+anims[i]+".png", 58, 55, entityHeight + 10, entityWidth + 53, 8, 62);
+        }
+
+        currentAnim = animations[IDLE];
+        currentDir = Direction.RIGHT;
+    }
+
+    @Override
+    protected void initHitbox() {
+        hitBox = new Rectangle2D.Float((float)pos.x + xDrawOffset, (float)pos.y + yDrawOffset, entityWidth, entityHeight);
+    }
+
+    @Override
+    public void update() {
+        if (health <= 0) {
+            dead(animations[DEATH]);
+            return;
+        }
+
+        // Action Locking & State Reset
+        if (attack && (inAir || landing)) attack = false;
+
+        // State Selection
+        if (checkForTakingHit()) {
+            takeHit(animations[TAKE_HIT]);
+        } else if (isHurt) {
+            currentAnim.updateAnimationTick();
+            if (currentAnim.isAnimationCompleted()) {
+                isHurt = false;
+                currentAnim.reset();
+            }
+        } else if (isIdle) {
+            idle(animations[IDLE]);
+        } else if (attack) {
+            attack(animations[ATTACK]);
+        } else if (isOutOfBorders(currentAnim.getWidth())) {
+            updateFromCorners(animations[RUN]);
+            ySpeed = 0;
+            inAir = false;
+        } else if (landing) {
+            landing(animations[RUN]);
+        } else if (inAir) {
+            var pair = jump(currentAnim, currentDir, RUN, RUN, RUN, SCALE);
+            currentAnim = pair.value0();
+            currentDir = pair.value1();
+        } else {
+            var pair = run(currentAnim, currentDir, RUN, IDLE, SCALE);
+            currentAnim = pair.value0();
+            currentDir = pair.value1();
+        }
+
+        // Physics & Gravity Update
+        if (!isOutOfBorders(currentAnim.getWidth()) && !isIdle && !attack && !isHurt) {
+            physicsUpdate(TAKE_HIT);
+        }
+
+        // Update Visuals
+        boolean alreadyUpdated = (isIdle || attack || landing || isHurt);
+        if (!alreadyUpdated) {
+            currentAnim.updateAnimationTick();
+        }
+
+        updateHitbox();
+    }
+
+    @Override
+    protected void updateHitbox() {
+        if (currentDir == Direction.RIGHT) {
+            hitBox.x = (float) pos.x + xDrawOffset;
+        } else {
+            hitBox.x = (float) pos.x + (currentAnim.getWidth() * SCALE - xDrawOffset - hitBox.width);
+        }
+        hitBox.y = (float)pos.y + yDrawOffset;
+    }
+
+    public void draw(Graphics g) {
+        BufferedImage imageToDraw = currentAnim.getAnimationImage(currentDir);
+        g.drawImage(imageToDraw, (int)pos.x, (int)pos.y,
+                (int)(currentAnim.getWidth()*SCALE), (int)(currentAnim.getHeight()*SCALE), null);
+
+        // For debugging the hitBox
+//        drawHitbox(g);
+    }
+}
+
